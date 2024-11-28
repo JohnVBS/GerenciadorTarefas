1c6934086b18e1b49d8e2ef9a5c4d0b5df42ecd8
JohnVBS <joaovitor.save@hotmail.com>
2024-11-28 20:44:48 -0300

    jooj

---
+++ src/br/edu/unisep/tarefas/model/Tarefas.java
@@ -0,0 +1,27 @@
+package br.edu.unisep.tarefas.model;
+
+public class Tarefas {
+    private String tarefa;
+
+    public Tarefas() {
+    }
+
+    public Tarefas(String tarefa) {
+        this.tarefa = tarefa;
+    }
+
+    public String getTarefa() {
+        return tarefa;
+    }
+
+    public void setTarefa(String tarefa) {
+        this.tarefa = tarefa;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefas{" +
+                "tarefa='" + tarefa + '\'' +
+                '}';
+    }
+}
